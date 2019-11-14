c0f57250e86ffe20a3200493a52d563bb9aae526
Nico Virgino <nicovirgino@gmail.com>
2019-11-14 15:10:01 -0600

    test

--- nico.js
+++ nico.js
@@ -1,18 +1,4 @@
 "use strict";
-//
-// fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/quickAnswer?q=How%20much%20vitamin%20c%20is%20in%202%20apples%3F", {
-//     "method": "GET",
-//     "headers": {
-//         "x-rapidapi-host": "https://api.spoonacular.com/recipes/search",
-//         "x-rapidapi-key": foodKey
-//     }
-// })
-//     .then(response => {
-//         console.log(response);
-//     })
-//     .catch(err => {
-//         console.log(err);
-//     });
 
 function getFoodKey() {
     fetch(`https://api.spoonacular.com/recipes/search?apiKey=${foodKey}&query=cheese&number=2`)
@@ -30,5 +16,6 @@ function getFoodKey() {
 }
 
 getFoodKey();
+console.log("git test");
 
 
