cf61d34443320a92bfc44b30a17c2334eda5f907
Nico Virgino <nicovirgino@gmail.com>
2019-11-21 11:11:50 -0600

    changed some stuff...

--- js/nico.js
+++ js/nico.js
@@ -1,22 +1,22 @@
-// "use strict";
-//
-// function getFoodKey() {
-//     fetch(`https://api.spoonacular.com/recipes/search?apiKey=${foodKey}&query=cheese&number=2`)
-//         .then(response => response.json())
-//         .then(element => {
-//             console.log(element);
-//             let ready = element;
-//             console.log(ready)
-//
-//
-//         }).catch(err => {
-//         console.log(err);
-//     })
-//
-// }
-//
-//
-// getFoodKey();
-// console.log("git test");
+"use strict";
+
+function getFoodKey() {
+    fetch(`https://api.spoonacular.com/recipes/search?apiKey=${foodKey}&query=wine&`)
+        .then(response => response.json())
+        .then(element => {
+            console.log(element);
+            let ready = element;
+            console.log(ready)
+
+
+        }).catch(err => {
+        console.log(err);
+    })
+
+}
+
+
+getFoodKey();
+console.log("git test");
 
 
