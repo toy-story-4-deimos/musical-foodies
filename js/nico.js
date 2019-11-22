0dc81b5afd1c5cb224341e3363ab81d3b35bb3b6
Nico Virgino <nicovirgino@gmail.com>
2019-11-21 22:54:38 -0600

    added more html shenanigans, navbar sticky, font, playingwith color schemes, rows/columns, css, jquery, images, aesthetics, company name etc.

--- js/nico.js
+++ js/nico.js
@@ -1,22 +1,21 @@
 "use strict";
-
-function getFoodKey() {
-    fetch(`https://api.spoonacular.com/recipes/search?apiKey=${foodKey}&query=wine&`)
-        .then(response => response.json())
-        .then(element => {
-            console.log(element);
-            let ready = element;
-            console.log(ready)
-
-
-        }).catch(err => {
-        console.log(err);
-    })
-
-}
-
-
-getFoodKey();
-console.log("git test");
+//
+// function getFoodKey() {
+//     fetch(`https://api.spoonacular.com/recipes/search?apiKey=${foodKey}&query=wine&`)
+//         .then(response => response.json())
+//         .then(element => {
+//             console.log(element);
+//             let ready = element;
+//             console.log(ready)
+//         })
+//         .catch(err => {
+//         console.log(err);
+//     })
+//
+// }
+//
+//
+// getFoodKey();
+// console.log("git test");
 
 
